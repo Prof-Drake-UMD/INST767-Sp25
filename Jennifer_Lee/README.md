f08bc02f2ecc6cb6e794214e1b6bc28c31e3e73a
jennyyleee <jennifergawon@gmail.com>
2025-04-01 21:23:20 -0400

    updating notebook

--- Jennifer_Lee/README.md
+++ Jennifer_Lee/README.md
@@ -1,8 +1,12 @@
 For my project, I will be creating a DAG for financial market data. 
 
-I will be using https://publicapis.io/yahoo-finance-api, https://publicapis.io/coin-gecko-api, and https://publicapis.io/fred-api.
-The first API is Yahoo Finance, and it consists of financial market data. 
-The second API is CoinGecko and contains information on cryptocurrency prices.
-The third API is the Federal Reserve Economic Data and has macroeconomic data. 
+I will be using https://github.com/ranaroussi/yfinance, https://publicapis.io/coin-gecko-api, and https://publicapis.io/fred-api.
 
-I will use all of these APIs to analyze trends and correlations in the stock market throughout time and predict trends. 
+The first API contains data on Yahoo Finance, and it consists of financial market data. The inputs will be ticker symbols, and the expected outputs will be information on stocks such as historical prices and volume. 
+
+The second API is CoinGecko and contains information on cryptocurrency prices. The inpus will be cryptocurrency IDs, and the expected outputs will be the price (in USD), market cap, and volume. 
+
+The third API is the Federal Reserve Economic Data and has macroeconomic data. The inputs will be Series ID, and the expected outputs will be time-series data for macroeconomic indictors. 
+
+I will use all of these APIs to analyze trends in the stock and crypto market and study correlations with macroeconomic factors. I will then model and forecast future movements in the market. 
+
---
+++ Jennifer_Lee/ingest.py
@@ -0,0 +1,74 @@
+!pip install yfinance
+import os
+import requests
+import yfinance as yf
+
+#yahoo finance 
+
+def fetch_yahoo(ticker_symbol="AAPL", period='1mo', interval='1d'):
+    ticker = yf.Ticker(ticker_symbol)
+    data = ticker.history(period=period, interval=interval)
+    return data
+
+
+
+if __name__ == "__main__":
+    stock_data = fetch_yahoo("AAPL")
+    print("Stock Data:\n", stock_data.head())
+
+    def fetch_yahoo_as_json(ticker_symbol="AAPL", period="1mo", interval="1d"):
+    
+    ticker = yf.Ticker(ticker_symbol)
+    data = ticker.history(period=period, interval=interval)
+
+    # converting dataframe to JSON string
+    json_data = data.to_json(orient="records", date_format="iso")
+
+    # converting to python object
+    json_object = json.loads(json_data)
+
+    return json_object
+
+# saving to file 
+def save_to_json_file(data, filename="finance_data.json"):
+    with open(filename, "w") as f:
+        json.dump(data, f, indent=4)
+    print(f"Data saved to {filename}")
+
+if __name__ == "__main__":
+    stock_data = fetch_yahoo_as_json("AAPL")
+    save_to_json_file(stock_data, "aapl_stock_data.json")
+    
+#CoinGecko
+
+def fetch_coingecko(crypto_id="bitcoin"):
+    
+    url = f"https://api.coingecko.com/api/v3/simple/price"
+    params = {
+        "ids": crypto_id,
+        "vs_currencies": "usd",
+        "include_market_cap": "true",
+        "include_24hr_vol": "true",
+        "include_24hr_change": "true"
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+if __name__ == "__main__":
+    crypto_data = fetch_coingecko("bitcoin")
+    print("Crypto Data:\n", crypto_data)
+    
+def fetch_fred(series_id="CPIAUCSL", api_key= "3ab53daa5812f18f00c5b0873399a5bc"):
+  
+    url = "https://api.stlouisfed.org/fred/series/observations"
+    params = {
+        "series_id": series_id,
+        "api_key": api_key,
+        "file_type": "json"
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+if __name__ == "__main__":
+    fred_data = fetch_fred("CPIAUCSL", api_key="3ab53daa5812f18f00c5b0873399a5bc")
+    print("FRED Data:\n", fred_data["observations"][:5])
