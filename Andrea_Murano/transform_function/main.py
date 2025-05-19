c9ff42cc630b424d76b605a4b59258e7b1ca11eb
Andrea Murano <akmurano91@gmail.com>
2025-05-19 19:52:23 -0400

    Update main.py

--- Andrea_Murano/transform_function/main.py
+++ Andrea_Murano/transform_function/main.py
@@ -1,17 +1,8 @@
-import os
-import sys
 import base64
 import json
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from api_logic import flatten_and_export
 
 def main(event, context):
-    """
-    Google Cloud Function entry point.
-    Triggered by Pub/Sub when ingest function publishes a message.
-    Receives data, then flattens and exports it (e.g., to BigQuery or Cloud Storage).
-    """
     try:
         raw_data = base64.b64decode(event['data']).decode('utf-8')
         result = json.loads(raw_data)
@@ -20,7 +11,7 @@ def main(event, context):
         return
 
     try:
-        flatten_and_export(result) 
+        flatten_and_export(result)
         print("Flattened and exported result.")
     except Exception as e:
         print(f"Error flattening/exporting: {e}")
