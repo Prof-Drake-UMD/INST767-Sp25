ec7fe6fb274d223ae7e983c21f990090482daa64
avkash10 <102694192+avkash10@users.noreply.github.com>
2025-05-19 14:39:47 -0400

    Update create_tables.py
    different table such as merged songs i could use

--- avkash_chandra/sql/create_tables.py
+++ avkash_chandra/sql/create_tables.py
@@ -1,15 +1,19 @@
 schema_sql = """
-CREATE TABLE song_insights (
+CREATE TABLE song_weather (
     song_title STRING,
     artist STRING,
-    lyrics_url STRING,
+    city STRING,
+    weather_main STRING,
+    temperature FLOAT64,
+    weather_description STRING,
+    lyrics_preview STRING,
+    song_mood STRING,
+    weather_mood STRING,
     lastfm_listeners INT64,
     lastfm_playcount STRING,
-    temperature_F FLOAT64,
-    weather_main STRING,
-    weather_description STRING,
-    city STRING
+    ingested_at TIMESTAMP
 );
+
 """
 
 with open("../Library/Application Support/JetBrains/PyCharm2025.1/scratches/create_tables.sql", "w") as f:
