d01c79b7bf2fc9da3e3c157fb59115d8f3bffee0
Andrea Murano <akmurano91@gmail.com>
2025-05-20 07:59:05 -0400

    Update and rename Andrea_Murano_sample_queries_Version5.sql to queries.sql

--- Andrea_Murano/queries.sql
+++ Andrea_Murano/queries.sql
@@ -1,4 +1,4 @@
--- 1. Find all art and music from the same year as a selected book
+------------------------------ 1. Find all art and music from the same year as a selected book ------------------------------
 WITH book_info AS (
   SELECT * FROM `inst767-murano-cultural-lens.cultural_lens.books`
   WHERE title = 'Normal People'
@@ -16,20 +16,20 @@ SELECT
   m.album,
   m.release_date
 FROM book_info b
-LEFT JOIN `inst767-murano-cultural-lens.cultural_lens.artworks` a
+LEFT JOIN `inst767-murano-cultural-lens.cultural_lens.artwork` a
   ON SAFE_CAST(a.object_date AS INT64) = b.first_publish_year
 LEFT JOIN `inst767-murano-cultural-lens.cultural_lens.music` m
   ON EXTRACT(YEAR FROM m.release_date) = b.first_publish_year
 ORDER BY a.title, m.title;
 
--- 2. Top artists and authors for a given year
+------------------------------ 2. Top artists and authors for a given year ------------------------------
 SELECT
   b.author_name AS book_author,
   a.artist_name AS artwork_artist,
   m.artist AS music_artist,
   COUNT(*) AS count
 FROM `inst767-murano-cultural-lens.cultural_lens.books` b
-LEFT JOIN `inst767-murano-cultural-lens.cultural_lens.artworks` a
+LEFT JOIN `inst767-murano-cultural-lens.cultural_lens.artwork` a
   ON SAFE_CAST(a.object_date AS INT64) = b.first_publish_year
 LEFT JOIN `inst767-murano-cultural-lens.cultural_lens.music` m
   ON EXTRACT(YEAR FROM m.release_date) = b.first_publish_year
@@ -38,13 +38,13 @@ GROUP BY book_author, artwork_artist, music_artist
 ORDER BY count DESC
 LIMIT 10;
 
--- 3. List all works (books, art, music) from a given year
+------------------------------ 3. List all works (books, art, music) from a given year ------------------------------
 SELECT 'Book' AS type, title, author_name AS creator, first_publish_year AS year
 FROM `inst767-murano-cultural-lens.cultural_lens.books`
 WHERE first_publish_year = 2018
 UNION ALL
 SELECT 'Artwork', title, artist_name, SAFE_CAST(object_date AS INT64)
-FROM `inst767-murano-cultural-lens.cultural_lens.artworks`
+FROM `inst767-murano-cultural-lens.cultural_lens.artwork`
 WHERE SAFE_CAST(object_date AS INT64) = 2018
 UNION ALL
 SELECT 'Music', title, artist, EXTRACT(YEAR FROM release_date)
