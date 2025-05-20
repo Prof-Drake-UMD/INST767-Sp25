82328513323dfe722c34380a6fa92d99940d857c
Andrea Murano <akmurano91@gmail.com>
2025-05-20 06:54:23 -0400

    Update api_logic.py

--- Andrea_Murano/ingest_function/api_logic.py
+++ Andrea_Murano/ingest_function/api_logic.py
@@ -5,10 +5,15 @@ import random
 import re
 from datetime import datetime
 from dotenv import load_dotenv
+
 load_dotenv()
 
-SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+GCP_PROJECT = os.getenv("GCP_PROJECT")
+GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
+PUBSUB_TOPIC = os.getenv("PUBSUB_TOPIC")
+
 
 def get_spotify_token():
     auth_url = "https://accounts.spotify.com/api/token"
