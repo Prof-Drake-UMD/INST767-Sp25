22e899ac884b4747434d4986886f6b5c82dfef6f
Andrea Murano <akmurano91@gmail.com>
2025-05-20 07:52:33 -0400

    Add files via upload

---
+++ Andrea_Murano/create_tables.sql
@@ -0,0 +1,33 @@
+-- Books Table
+CREATE TABLE `inst767-murano.cultural_lens.books` (
+  book_id STRING,
+  title STRING,
+  author_name STRING,
+  first_publish_year INT64,
+  language STRING,
+  book_url STRING,
+  ingest_ts TIMESTAMP
+);
+
+-- Artworks Table
+CREATE TABLE `inst767-murano.cultural_lens.artworks` (
+  object_id INT64,
+  title STRING,
+  artist_name STRING,
+  medium STRING,
+  object_date STRING,
+  object_url STRING,
+  image_url STRING,
+  ingest_ts TIMESTAMP
+);
+
+-- Music Table
+CREATE TABLE `inst767-murano.cultural_lens.music` (
+  track_id STRING,
+  title STRING,
+  artist STRING,
+  album STRING,
+  release_date DATE,
+  preview_url STRING,
+  ingest_ts TIMESTAMP
+);
