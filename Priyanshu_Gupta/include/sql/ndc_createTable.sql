5dd8ddee7eee3081ba1a50bd8fd7408a29dd72d0
priyanshu124 <79084050+priyanshu124@users.noreply.github.com>
2025-05-19 23:24:21 -0400

    Update ndc_createTable.sql

---
+++ Priyanshu_Gupta/include/sql/ndc_createTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `inst767-openfda-pg.openfda.ndc-directory` (
+  product_ndc STRING,
+  generic_name STRING,
+  brand_name STRING,
+  labeler_name STRING,
+  dosage_form STRING,
+  route STRING,  
+  marketing_category STRING,
+  marketing_start_date DATE
+)
