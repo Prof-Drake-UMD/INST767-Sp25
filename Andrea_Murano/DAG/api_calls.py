c16d46f2dc98f56b384596ec14a42116c56e2666
Andrea Murano <akmurano91@gmail.com>
2025-05-20 08:26:02 -0400

    Update api_calls.py

--- Andrea_Murano/DAG/api_calls.py
+++ Andrea_Murano/DAG/api_calls.py
@@ -21,7 +21,7 @@ def fetch_books(title="Normal People", author="Sally Rooney"):
                 })
     return books
 
-# artwork: The Met
+# ARTWORK: The Met
 def fetch_artwork(year, buffer=10):
     search_url = "https://collectionapi.metmuseum.org/public/collection/v1/search"
     object_url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
